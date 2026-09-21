a7493ad1b8336a691552a99051b831ed79c13906
OrhanDev16 <orhammamashov1611@gmail.com>
2026-09-21 14:12:28 +0600

    I added Product

--- my_site/store_app/models.py
+++ my_site/store_app/models.py
@@ -3,3 +3,10 @@ from django.db import models
 
 class Category(models.Model):
     category_name = models.CharField(max_length=130, unigue=True)
+
+
+
+class Product(models.Model):
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    product_name = models.CharField(max_length=100)
+    description = models.TextField(null=True, blank=True)
